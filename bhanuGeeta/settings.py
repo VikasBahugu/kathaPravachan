15a5fa9102880986e1821ba623f2310a63768ac3
VikasBahugu <48058253+VikasBahugu@users.noreply.github.com>
2020-08-23 21:08:52 +0100

    Update settings.py
    In this commit, I have secured my password because it is public repository.

--- bhanuGeeta/settings.py
+++ bhanuGeeta/settings.py
@@ -138,4 +138,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bhanuastrology@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bhanu@astrology'
+EMAIL_HOST_PASSWORD = 'your email password'
